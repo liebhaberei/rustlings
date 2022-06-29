cb94914ae9dc4d9fd27ec5b12bcec122af5afb4d
Thomux <me@homux.eu>
2022-06-29 17:27:09 +0200

    more exercises

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -26,8 +26,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -41,6 +39,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let parts: Vec<&str> = s.split(",").collect();
+        if parts.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = if parts[0].len() > 0 {
+            parts[0].to_string()
+        } else {
+            return Err(ParsePersonError::NoName);
+        };
+
+        let age = match parts[1].parse::<usize>() {
+            Ok(age) => age,
+            Err(e) => return Err(ParsePersonError::ParseInt(e)),
+        };
+
+        Ok(Person { name, age })
     }
 }
 
